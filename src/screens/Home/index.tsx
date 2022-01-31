550bd2914df449d5e62ce774c7ba34548a9a2563
matheuseufrasio2 <matheuseufrasio2@gmail.com>
2022-01-31 17:21:06 -0300

    Typescript config + theme config + assets added

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+  View
+} from 'react-native';
+
+import { Container, Title } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  );
+}
