ebec62b969bf72d8079c41042be3352d15ae8bdd
matheuseufrasio2 <matheuseufrasio2@gmail.com>
2022-02-22 15:32:46 -0300

    tela SchedulingComplete finalized

--- App.tsx
+++ App.tsx
@@ -19,6 +19,9 @@ import { Home } from './src/screens/Home';
 import theme from './src/styles/theme';
 import { CarDetails } from './src/screens/CarDetails';
 import { StatusBar } from 'expo-status-bar';
+import { Scheduling } from './src/screens/Scheduling';
+import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -36,7 +39,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar />
-      <CarDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
